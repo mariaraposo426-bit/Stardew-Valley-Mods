0332a7576e580ae08b13038d3389ab7464bbea70
mariaraposo426-bit <mariaraposo426@gmail.com>
2025-10-29 17:02:41 +0000

    Update CustomFurnitureMod.cs

--- CustomFurniture/CustomFurnitureMod.cs
+++ CustomFurniture/CustomFurnitureMod.cs
@@ -1,4 +1,4 @@
-﻿using StardewModdingAPI;
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using Microsoft.Xna.Framework;
 using System.IO;
@@ -12,7 +12,8 @@ using HarmonyLib;
 using System.Reflection;
 using Microsoft.Xna.Framework.Graphics;
 using SpaceShared.APIs;
-using System.Threading.Tasks;
+using StardewValley.GameData.Shops;
+using StardewValley.Internal;
 
 namespace CustomFurniture
 {
@@ -111,55 +112,6 @@ namespace CustomFurniture
             Game1.spriteBatch.Draw(texture, location, sourceRectangle, color, rotation, origin, scale, spriteeffects, layerDepth);
         }
 
-        private void replaceCustomFurniture(string action, string[] param)
-        {
-            if (param[0] == "itemMenu" && Game1.activeClickableMenu is ItemGrabMenu itemMenu)
-            {
-                List<Item> remove = new List<Item>();
-                List<Item> additions = new List<Item>();
-
-                foreach (Item item in itemMenu.ItemsToGrabMenu.actualInventory)
-                    if (item is Chest chest && furniturePile.Keys.Where(f => f.Equals(item.Name)).Any())
-                    {
-                        Item cf = furniturePile[furniturePile.Keys.Where(f => f.Equals(item.Name)).FirstOrDefault()];
-                        additions.Add(cf);
-                        remove.Add(item);
-                    }
-
-                foreach (Item addition in additions)
-                    itemMenu.ItemsToGrabMenu.actualInventory.Add(addition);
-
-                foreach (Item j in remove)
-                    itemMenu.ItemsToGrabMenu.actualInventory.Remove(j);
-            }
-
-            if (param[0] == "shop" && Game1.activeClickableMenu is ShopMenu shop)
-            {
-                Dictionary<ISalable, int[]> items = Helper.Reflection.GetField<Dictionary<ISalable, int[]>>(shop, "itemPriceAndStock").GetValue();
-                List<ISalable> selling = Helper.Reflection.GetField<List<ISalable>>(shop, "forSale").GetValue();
-                List<ISalable> remove = new List<ISalable>();
-                List<ISalable> additions = new List<ISalable>();
-
-                foreach (Item i in selling)
-                    if (i is Chest chest && furniturePile.Keys.Any(f => f.Equals(i.Name)))
-                    {
-                        Item cf = furniturePile[furniturePile.Keys.FirstOrDefault(f => f.Equals(i.Name))];
-                        items.Add(cf, new int[] { chest.preservedParentSheetIndex.Value, int.MaxValue });
-                        additions.Add(cf);
-                        remove.Add(i);
-                    }
-
-                foreach (Item addition in additions)
-                    selling.Add(addition);
-
-                foreach (Item j in remove)
-                {
-                    items.Remove(j);
-                    selling.Remove(j);
-                }
-            }
-        }
-
         public static void log(string text)
         {
             instance.Monitor.Log(text,LogLevel.Trace);
@@ -167,12 +119,81 @@ namespace CustomFurniture
 
         private void OnReturnedToTitle(object sender, ReturnedToTitleEventArgs e)
         {
-            Helper.Events.Display.MenuChanged -= OnMenuChanged;
+            Helper.Events.Content.AssetRequested -= OnAssetRequested;
         }
 
         private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
         {
-            Helper.Events.Display.MenuChanged += OnMenuChanged;
+            Helper.Events.Content.AssetRequested += OnAssetRequested;
+        }
+
+        private void OnAssetRequested(object sender, AssetRequestedEventArgs e)
+        {
+            // Add custom furniture to shop data
+            if (e.NameWithoutLocale.IsEquivalentTo("Data/Shops"))
+            {
+                e.Edit(asset =>
+                {
+                    var data = asset.AsDictionary<string, ShopData>().Data;
+                    
+                    // Add to furniture catalogue
+                    if (data.TryGetValue("Catalogue", out var catalogueData))
+                    {
+                        AddFurnitureToShop(catalogueData, isCatalogue: true);
+                    }
+                    
+                    // Add to Robin's shop
+                    if (data.TryGetValue("Carpenter", out var carpenterData))
+                    {
+                        AddFurnitureToShop(carpenterData, shopkeeper: "Robin");
+                    }
+                    
+                    // You can add more shops here as needed
+                });
+            }
+        }
+
+        private void AddFurnitureToShop(ShopData shopData, bool isCatalogue = false, string shopkeeper = null)
+        {
+            if (shopData.Items == null)
+                shopData.Items = new List<ShopItemData>();
+
+            foreach (CustomFurniture f in furniture.Values)
+            {
+                if (!f.data.sellAtShop)
+                    continue;
+
+                // Check conditions if specified
+                if (f.data.conditions != "none" && !string.IsNullOrEmpty(f.data.conditions))
+                {
+                    // Skip for now - conditions would need to be converted to 1.6 format
+                    continue;
+                }
+
+                // Check if this furniture should be in this shop
+                bool shouldAdd = false;
+                if (isCatalogue)
+                {
+                    shouldAdd = true;
+                }
+                else if (shopkeeper != null && f.data.shopkeeper == shopkeeper)
+                {
+                    shouldAdd = true;
+                }
+
+                if (shouldAdd)
+                {
+                    var shopItem = new ShopItemData
+                    {
+                        ItemId = f.ItemId,
+                        Price = isCatalogue ? 0 : f.data.price,
+                        AvailableStock = int.MaxValue,
+                        IsRecipe = false
+                    };
+
+                    shopData.Items.Add(shopItem);
+                }
+            }
         }
 
         private Api api;
@@ -204,14 +225,22 @@ namespace CustomFurniture
 
                     CustomFurniturePack pack = cpack.ReadJsonFile<CustomFurniturePack>(Path.GetFileName(file));
 
+                    if (pack == null)
+                    {
+                        Monitor.Log($"Failed to load pack from {file}", LogLevel.Warn);
+                        continue;
+                    }
+
                     pack.author = cpack.Manifest.Author;
                     pack.version = cpack.Manifest.Version.ToString();
                     string author = pack.author == "none" ? "" : " by " + pack.author;
                     Monitor.Log(pack.name + " " + pack.version + author, LogLevel.Info);
+                    
                     if (!furnitureByContentPack.ContainsKey(cpack.Manifest))
                     {
                       furnitureByContentPack.Add(cpack.Manifest, new List<string>());
                     }
+                    
                     foreach (CustomFurnitureData data in pack.furniture)
                     {
                         countObjects++;
@@ -220,6 +249,7 @@ namespace CustomFurniture
                         string objectID = pileID;
                         CustomFurnitureMod.log("Load:" + objectID);
                         string tkey = $"{data.folderName}/{ data.texture}";
+                        
                         if (data.textureOverlay != null)
                         {
                             string tkey2 = $"{data.folderName}/{ data.textureOverlay}";
@@ -236,12 +266,20 @@ namespace CustomFurniture
 
                         if (!CustomFurniture.Textures.ContainsKey(tkey))
                             CustomFurniture.Textures.Add(tkey, data.fromContent ? data.texture : cpack.ModContent.GetInternalAssetName(data.texture).Name);
+                        
                         CustomFurniture f = new CustomFurniture(data, objectID, Vector2.Zero);
                         furniturePile.Remove(pileID);
                         furniture.Remove(objectID);
                         furniturePile.Add(pileID, f);
                         furniture.Add(objectID, f);
-                        furnitureByContentPack[cpack.Manifest].Add(f.name);
+                        furnitureByContentPack[cpack.Manifest].Add(f.Name);
+                        
+                        // Handle instant gifts
+                        if (f.data.instantGift != "none")
+                        {
+                            // This would need to be handled differently in 1.6
+                            // possibly through mail or other systems
+                        }
                     }
                 }
 
@@ -257,57 +295,16 @@ namespace CustomFurniture
 
         private bool meetsConditions(string conditions)
         {
-            return (Helper.Reflection.GetMethod(Game1.currentLocation, "checkEventPrecondition").Invoke<int>("9999984/" + conditions) != -1);
-        }
-
-        private void OnMenuChanged(object sender, MenuChangedEventArgs e)
-        {
-            if (Game1.activeClickableMenu is ShopMenu)
+            // SDV 1.6: Condition checking has changed significantly
+            // This would need to be rewritten using GameStateQuery
+            try
             {
-                ShopMenu shop = (ShopMenu)Game1.activeClickableMenu;
-                Dictionary<ISalable, int[]> items = Helper.Reflection.GetField<Dictionary<ISalable, int[]>>(shop, "itemPriceAndStock").GetValue();
-                List<ISalable> selling = Helper.Reflection.GetField<List<ISalable>>(shop, "forSale").GetValue();
-                int currency = Helper.Reflection.GetField<int>(shop, "currency").GetValue();
-                bool isCatalogue = (currency == 0 && selling.Count > 0 && selling[0] is Furniture);
-                string shopkeeper = "Robin";
-
-
-                if (shop.portraitPerson != null || isCatalogue)
-                {
-                    Dictionary<ISalable, int> newItemsToSell = new Dictionary<ISalable, int>();
-
-                    foreach (CustomFurniture f in furniture.Values)
-                    {
-                        if (!f.data.sellAtShop || (f.data.conditions != "none" && !meetsConditions(f.data.conditions)))
-                            continue;
-
-                        if (Game1.getCharacterFromName(f.data.shopkeeper) is NPC sk && !sk.IsInvisible)
-                            shopkeeper = f.data.shopkeeper;
-                        else
-                            shopkeeper = "Robin";
-
-                        if (f.data.instantGift != "none")
-                        {
-                            Game1.player.addItemByMenuIfNecessary(f);
-                            Game1.player.mailReceived.Remove(f.data.instantGift);
-                            continue;
-                        }
-
-                        if ((shop.portraitPerson is NPC shopk && shopk.Name == shopkeeper) || isCatalogue)
-                                newItemsToSell.Add(f, isCatalogue ? 0 : f.Price);
-                    }
-
-                    foreach (ISalable item in newItemsToSell.Keys)
-                        if (!items.ContainsKey(item))
-                        {
-                            items.Add(item, new int[] { newItemsToSell[item], int.MaxValue });
-                            selling.Add(item);
-                        }
-                }
-
+                return GameStateQuery.CheckConditions(conditions);
+            }
+            catch
+            {
+                return false;
             }
         }
-
-        
     }
 }
